b10be77752b6b3ba2b8f8d5942504566dedb5905
korewest <korewest@gmail.com>
2015-09-12 23:41:27 -0700

    Visual improvements / additional photos

---
+++ main/js/nav.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    // Scroll to proper sections indicated by nav block
+    $('.navblock').click(function() {
+        var div_scroll = $(this).attr('id');
+        $('html,body').animate({
+            scrollTop: $("#" + div_scroll + "Seg").offset().top - 88 /* Offset to match seg */
+        }, 500);
+    });
+    
+    $('#khl').click(function() {
+        $('html,body').animate({
+            scrollTop: $("#andrew_img").offset().top }, 500);
+    });
+});
---
+++ main/js/photo-change.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    
+    var numPhotos = 4;
+    var currPhoto = 0;
+    
+    function changePhoto() {
+        
+        var nextPhoto = currPhoto + 1;
+        
+        if( currPhoto == numPhotos ) {
+            nextPhoto = 0;
+        }
+        
+        $("#photographySeg").css("opacity", "0.4");
+        $("#photographySeg").animate({ "opacity" : "1.0" }, 2000 )
+        
+        $("#photographySeg").removeClass("photoSegBackground" + currPhoto.toString());
+        $("#photographySeg").addClass("photoSegBackground" + nextPhoto.toString());
+        
+        currPhoto = nextPhoto;
+    }
+    
+    window.setInterval( changePhoto, 5000 );
+});

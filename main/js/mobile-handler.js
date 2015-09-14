f1e60fb8b4602247ecee955f939d5dd627d848b8
korewest <korewest@gmail.com>
2015-09-14 01:46:40 -0700

    Hide content if less than 750px while mobile development is taking place

--- main/js/mobile-handler.js
+++ main/js/mobile-handler.js
@@ -3,6 +3,11 @@
 $(document).ready(function() {
     var $window = $(window);
     
+    // some variable initializations on load
+    var temp_html = $("#name_text").html();
+    // note: re-enable when mobile version is complete
+    // $("#mobile_name_text").html(temp_html);
+    
     function checkWidth() { 
         var windowSize = $window.width();
         
@@ -19,10 +24,15 @@ $(document).ready(function() {
             $("#andrew_img").addClass("andrew_img_mobile");
             // hide text in the img box
             $("#name_text").hide();
-            var temp_html = $("#name_text").html();
-            $("#mobile_name_text").html(temp_html);
-            $("#mobile_name_text").show();
             
+            $("#mobile_welcome").show();
+            
+            
+            // Temporarily hide other contents while mobile dev is underway
+            $("#chevronDiv").hide();
+            $("#aboutSeg").hide();
+            $("#photographySeg").hide();
+            $("#whatSeg").hide();
             
         } else {
             // Restore to normal (in case of window resize)
@@ -31,9 +41,37 @@ $(document).ready(function() {
             
             $("#andrew_img").removeClass("andrew_img_mobile");
             $("#name_text").show();
-            $("#mobile_name_text").hide();
+            $("#mobile_welcome").hide();
+            
+            
+            $("#chevronDiv").show();
+            $("#aboutSeg").show();
+            $("#photographySeg").show();
+            $("#whatSeg").show();
         }
     }
+    
+    // Mobile does not like css hover
+    $("#mobile_welcome")
+        .mousedown(function() { 
+            $("#mobile_welcome").css("opacity", "0.95");
+        })
+        .mouseover(function() {
+            $("#mobile_welcome").css("opacity", "0.95");
+            $("#mobile_welcome").css("cursor", "pointer");
+        })
+        .mouseout(function() {
+            $("#mobile_welcome").css("opacity", "0.8");
+            $("#mobile_welcome").css("cursor", "default");
+    });
+    
+    $("#mobile_welcome").click(function() {
+        if( $("#mobile_name_text").is(":hidden") ) {
+            $("#mobile_name_text").slideDown("slow");
+        } else {
+            $("#mobile_name_text").slideUp("slow");
+        }
+    });
     
     // check width on load
     checkWidth();
